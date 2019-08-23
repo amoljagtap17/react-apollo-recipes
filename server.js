9773eb76d0db43e024bd97a3911070012b8c499c
Amol Jagtap <amoljagtap17@gmail.com>
2019-08-23 22:19:12 +0530

    REACT-APOLLO-RECIPES: Connect to MongoDB using mongoose

--- server.js
+++ server.js
@@ -1,4 +1,11 @@
 const express = require('express')
+const mongoose = require('mongoose')
+require('dotenv').config({ path: 'dev.env' })
+
+mongoose
+  .connect(process.env.MONGO_URI, { useNewUrlParser: true })
+  .then(() => console.log('DB connected!'))
+  .catch(err => console.error(err))
 
 const app = express()
 
