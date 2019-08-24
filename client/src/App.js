0f82d67ba249952ea62a50c3c3df1005df718cdb
Amol Jagtap <amoljagtap17@gmail.com>
2019-08-24 16:29:25 +0530

    REACT-APOLLO-RECIPES: Create Apollo Client instance and add Apollo Provider Wrapper to app

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,15 @@
 import React from 'react'
+import { ApolloProvider } from '@apollo/react-hooks'
+import apolloClient from './config/createApolloClient'
 
 function App() {
-  return <div>App!</div>
+  return (
+    <ApolloProvider client={apolloClient}>
+      <div>
+        <h1>React Apollo Client</h1>
+      </div>
+    </ApolloProvider>
+  )
 }
 
 export default App
