6d831fc7e90310cbc8f37d48383fd290edc2eb87
Amol Jagtap <amoljagtap17@gmail.com>
2019-08-24 13:48:01 +0530

    REACT-APOLLO-RECIPES: Add Apollo Server Express middleware

--- server.js
+++ server.js
@@ -5,6 +5,11 @@ require('dotenv').config({ path: 'dev.env' })
 const Recipe = require('./models/Recipe')
 const User = require('./models/User')
 
+const { ApolloServer } = require('apollo-server-express')
+
+const { typeDefs } = require('./schema')
+const { resolvers } = require('./resolvers')
+
 // Connect to database
 mongoose
   .connect(process.env.MONGO_URI, { useNewUrlParser: true })
@@ -19,8 +24,17 @@ mongoose.set('useCreateIndex', true)
 // Initialize Express application
 const app = express()
 
+const apolloServer = new ApolloServer({
+  typeDefs,
+  resolvers
+})
+
+apolloServer.applyMiddleware({ app, path: '/api' })
+
 const PORT = process.env.PORT || 4444
 
 app.listen(PORT, () => {
-  console.log(`Server listening on PORT ${PORT}`)
+  console.log(
+    `Server listening on PORT ${PORT} with endpoint ${apolloServer.graphqlPath}`
+  )
 })
